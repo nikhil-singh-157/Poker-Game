c964b7ff12622a803175a8bcdf8dee6ae70ca06f
nikhil-singh-157 <67114627+nikhil-singh-157@users.noreply.github.com>
2020-06-20 12:33:23 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Poker Hands
+I have  used the Decks of Cards API https://deckofcardsapi.com to demonstrate the use of AJAX data retrieval.
+
+# run
+ run index.html in any browser.
---
+++ index.js
@@ -0,0 +1,34 @@
+// req-005 wrap the entire function in IIFE
+(function(){
+    // req-001 retrieve a deck of cards
+     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            let deckID = myJson.deck_id;
+            // req-002 request 5 cards from the deck
+            return fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=52`);
+            
+        })
+        .then((response2) => {
+            return response2.json();
+        })
+        .then((myDeck) => {
+            
+
+            // display highest poker hand to page
+            for(let i in myDeck.cards) {
+                if(myDeck.cards[i]) {
+                    document.getElementById("pokerhands").innerHTML += "<img class='transparent' src=" + myDeck.cards[i].image+ ">";
+                } else {
+                    document.getElementById("pokerhands").innerHTML += "<img src=" + myDeck.cards[i].image+ ">";
+                }
+            }
+            
+        }); 
+
+
+})();   // IIFE
+
+
